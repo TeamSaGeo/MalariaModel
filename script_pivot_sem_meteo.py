c1b82c10580272a72071ae73258e7a039802bbfd
famenontsoa <sfamenontsoa@gmail.com>
2023-08-16 11:23:21 +0300

    convert script_pivot_sem_meteo py to exe

--- script_pivot_sem_meteo.py
+++ script_pivot_sem_meteo.py
@@ -8,6 +8,6 @@ if os.path.exists(filepath):
   result_sorted = result.sort_values(by=["numero_annee", "Semaine"],ascending=True)
   new_path = os.path.splitext(filepath)[0] + "_pivoted.csv"
   result_sorted.to_csv(new_path, sep=";", index=False)
-  print("Pivot du données terminé avec succès!\nRésultat stocké dans le fichier" + new_path)
+  print("Pivot du données terminé avec succès!\nRésultat stocké dans le fichier " + new_path)
 else:
   print("Argument chemin du fichier de données météo source absent! Utiliser la commande suivante:\npython .\script_pivot_sem_meteo.py [chemin du fichier])")
